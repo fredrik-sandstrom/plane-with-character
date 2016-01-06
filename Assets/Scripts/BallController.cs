c4bb759e3ea866ddfbf831545911451ba1d8617c
Fredrik Sandstrom <fredrik.sandstrom@gmail.com>
2016-01-06 13:52:40 +0100

    Add initial stuff.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+  private Rigidbody rb;
+  public float speed;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+    rb.AddForce(movement * speed);
+  }
+
+}
---
+++ Assets/TetraController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetraController : MonoBehaviour {
+
+  private Rigidbody rb;
+  public float speed;
+  public GameObject[] thrusters;
+  public GameObject rearThruster;
+  public GameObject rightThruster;
+  public GameObject leftThruster;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  void Update()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    float thrust = 0.0f;
+
+    if (moveHorizontal < 0.01f)
+    {
+      //rb.AddForce( * speed);      
+    }
+
+    RaycastHit hit;
+    foreach (GameObject thruster in thrusters)
+    {
+      Ray thrustRay = new Ray(thruster.transform.position, Vector3.down);
+      if (Physics.Raycast(thrustRay, out hit, 2))
+      {
+        thrust = (2.0f - hit.distance) * (2.0f - hit.distance);        
+      }
+    }
+
+
+    Ray thrustRay2 = new Ray(transform.position, Vector3.down);
+    if (Physics.Raycast(thrustRay2, out hit, 2))
+    {
+      thrust = (2.0f - hit.distance) * (2.0f - hit.distance);
+    }
+
+    Vector3 movement = new Vector3(moveHorizontal, thrust, moveVertical);
+
+    rb.AddForce(movement * speed);
+  }
+
+}
